b6a82087cec22f6b4d859852064306614cd71c4b
Martin Luna <uxmoon@gmail.com>
2016-11-18 10:49:39 -0300

    updated more videos link height with js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
   'use strict';
+
+  var item = Math.floor($('.c-av__item').height());
+  $('.c-av__item-more a')
+   .css({
+    'height': (item + 'px'),
+    'line-height': (item + 'px')
+  });
+
 });
